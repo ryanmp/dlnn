fd10cead9b68716a2681a058d530d39eec9bf39f
Ryan Phillips <wordsandcolor@gmail.com>
2014-09-05 21:55:12 -0700

    getting it dialed in....

--- neural_nets/ffnet/run_me.py
+++ neural_nets/ffnet/run_me.py
@@ -5,25 +5,25 @@ Information here...
 '''
 
 # global libraries
-import scipy, numpy
+import scipy, numpy, random
 from scipy import stats
 
 # any global variables?
 ncols = 73
-nrows = 4100
+nrows = 1500
 
 # this is kind of a like a tolerance... training will continue until NF reaches this point
-max_functions = 300
+max_functions = 2210
 
 # anything smaller than nrows... and then save a few rows for a test (next line)
-training_set_size = 3500
+training_set_size = 1200
 
 # we run the net on the next n rows after the training set 
-in_sample_test_size = 50
+in_sample_test_size = 100
 
 
 # first and last must match in and out... but inner nodes can take any values
-network_config = (ncols-11,ncols,ncols,ncols,1)
+network_config = (ncols-11,12,1)
 
 
 def main():
@@ -37,8 +37,18 @@ def main():
 	print 'loading data from pickle'
 	sorted_data = pickle.load( open( "data.p", "rb" ) )
 	
-	forecast, real_data = build_ffnet(sorted_data)
-	return forecast, real_data, sorted_data
+	net = build_ffnet(sorted_data)
+
+	stats = []
+	for i in xrange(10):
+		tmp = calc_r2(net,sorted_data)
+		stats.append(tmp[0])
+		print tmp
+	print sum(stats)/len(stats)
+	
+	show_comp(net,sorted_data)
+
+	return net, sorted_data
 
 
 def process_data():
@@ -104,50 +114,69 @@ def build_ffnet(sorted_data):
 	conec = mlgraph(network_config) #skipping first 11 cols
 	net = ffnet(conec)
 
-	#print "Sets initial weights via genetic algo"
-	#net.train_genetic(data_in_training2, data_target_training2, individuals=3, generations=10)
-
-
 	print "TRAINING NETWORK..."
 	# that are many different training algos
 
 	#net.train_rprop(data_in_training2, data_target_training2, a=1.2, b=0.5, mimin=1e-06, mimax=50.0, xmi=0.1, maxiter=40, disp=1)
 	#net.train_momentum(data_in_training2, data_target_training2, eta=0.2, momentum=0.8, maxiter=200, disp=1)
-	net.train_tnc(data_in_training2, data_target_training2, maxfun = max_functions, nproc = cpu_count(), messages=1)
+	net.train_tnc(data_in_training2, data_target_training2, maxfun = max_functions, messages=1)
+	#net.train_cg(data_in_training2, data_target_training2, maxiter=5, disp=1)
+	#net.train_genetic(data_in_training2, data_target_training2, individuals=3, generations=300)
+	#net.train_bfgs(data_in_training2, data_target_training2, maxfun = 86999, disp=1)
 
-	# doing an 'in sample' prediction from our trained nn
-	data_input2 = sorted_data[training_set_size:training_set_size+in_sample_test_size,10:-2]
-	nn_ans = net(data_input2)
-	# shall we round it?
-	nn_ans = [i[0] for i in nn_ans]
-	#nn_ans = [round(i) for i in nn_ans]
+	return net
 
-	real_ans = sorted_data[training_set_size:training_set_size+in_sample_test_size,0].tolist()
+
+
+
+def calc_r2(net, sorted_data):
+
+	start = random.randrange(0,len(sorted_data)-in_sample_test_size)
+	end = start+in_sample_test_size
+
+	real_ans = sorted_data[start:end,0].tolist()
+	data_input2 = sorted_data[start:end,10:-2]
+	
+	nn_ans = [i[0] for i in net(data_input2)] # run the net
+	
+	#nn_ans = [round(i) for i in nn_ans] # shall we round it?
 
 	r_squared = scipy.stats.pearsonr(nn_ans, real_ans)[0]**2
-	print r_squared
+	return r_squared, start, end
 
-	return nn_ans, real_ans
 
-	'''
-	# do we want to graph it?
+def show_comp(net, sorted_data):
 
+	start = 0
+	end = len(sorted_data)-1
+
+	real_ans = sorted_data[start:end,0].tolist()
+	data_input2 = sorted_data[start:end,10:-2]
+	
+	nn_ans = [i[0] for i in net(data_input2)] # run the net
+	
 	import matplotlib.pyplot
 	import seaborn
 
 	fig = matplotlib.pyplot.figure()
 	ax1 = fig.add_subplot(211)
 
-	ax1.plot([i for i in xrange(len(nn_ans))],nn_ans, 'ro-', ms=.5)
-	ax1.plot([i for i in xrange(len(nn_ans))],real_ans, 'go-', ms=.5)
-	'''
+	ax1.plot([i for i in xrange(len(nn_ans))], nn_ans, 'o-', linewidth=.3, markersize=3, color="red")
+	ax1.plot([i for i in xrange(len(nn_ans))], real_ans, 'o-', linewidth=.3, markersize=3, color="blue")
 
+	diffs = [j-i for (i,j) in zip(nn_ans,real_ans)]
+	ax2 = fig.add_subplot(212)
+	ax2.plot([i for i in xrange(len(diffs))], diffs, 'o-', linewidth=.3, markersize=3)
+
+	fig.show()
+
+	return real_ans, nn_ans
 
 
 
 if __name__ == '__main__':
 	print 'starting main'
-	forecast, real_data, sorted_data = main()
+	net, sorted_data = main()
 
 
 
